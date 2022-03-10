0766131bd249d3aeb239e43477ef74bef36a5348
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-10 08:15:25 +0300

    C program that prints a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * main - entry point
+/**
+ * main - Entry point
  *
- * Return: always 0(success)
+ * Return: always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
