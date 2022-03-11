acb67c67d7514773baa54446a7bded2ca5d5a509
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-11 05:01:17 +0300

    Print all combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,10 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n; 	
-	for (n = 0; n <= 9; n++)
+
+	for (n = 0; n < 9; n++)
 	{
 		putchar(n + '0');
 		if (n < 9)
@@ -18,5 +20,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
+	
 	return (0);
 }
