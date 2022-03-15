66e96e368ff7c19a829e010ea090c69f933e93ce
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-15 04:34:13 +0300

    Checks is a chracter is an alphabet or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _isalpha - check if the charcter is an alphabet or not
- * @c - the charcter to be checked
+ * @c: the charcter to be checked
  *
  * Return: 1 if true or 0 if false
  */
 int _isalpha(int c)
 {
-	if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
+	if((c >= 'a' && c <= 'z') || 
+	   (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
