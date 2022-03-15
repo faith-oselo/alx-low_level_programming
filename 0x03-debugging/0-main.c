00aeb1bfa6935e4c157498351a97208ecacdeeaa
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-15 08:56:40 +0300

    Testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - tests if the function output is correct or not
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
