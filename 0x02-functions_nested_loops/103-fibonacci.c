ffff1a48923fd844531a9d8cf6147c704b4523dd
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-15 07:42:52 +0300

    Prints the sum of even-valued fibonacci sequence not exceeding 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,20 @@
 int main(void)
 {
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	long total_sum;
+	float total_sum;
 
-	while(1)
+	while (1)
 	{
 		sum = fib1 + fib2;
-		if (sum > 400000)
+		if (sum > 4000000)
 			break;
 		if ((sum % 2) == 0)
 			total_sum += sum;
+
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	printf("%lu\n", total_sum);
+	printf("%.0f\n", total_sum);
 
 	return (0);
 }
