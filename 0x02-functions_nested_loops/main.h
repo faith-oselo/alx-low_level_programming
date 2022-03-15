12744bd8974b31e39eb0aa226c4b9910f6d7bad2
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-15 03:36:33 +0300

    print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * file: main.h
+ *
+ * Desc: header file containing declarations for all functions
+ * 	used in 0x02-functions_nested_loops directory
+ */
+int _putchar(char c);
+
+#endif
