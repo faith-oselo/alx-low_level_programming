d86d63a75c4ae0f38ecff2ee9d64c2ff42480593
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-17 18:41:44 +0300

    error checking

--- 0x04-more_functions_nested_loops/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * Desc: header file containing prototypes for all functions
- *  used in the 0x04-more_functions_nested_loops
- */
-int _isupper(int c);
-
-#endif
