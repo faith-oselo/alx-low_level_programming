b1e958dc86d063afc8b96ee2af39020da87fd900
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-17 10:00:52 +0300

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 	{
 		for (h = 1; h <= size; h++)
 		{
-			for (i = size - h; i > 1; i--)
+			for (i = size - h; i > 0; i--)
 				_putchar(' ');
 			
 			for (i = 0; i < h; i++)
