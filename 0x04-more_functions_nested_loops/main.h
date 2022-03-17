e6c6c9f2d9c9fa09449fa71d85aa35f805d44216
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-17 08:56:18 +0300

    printing numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,8 +3,10 @@
 /*
  * Desc: Header file containing prototypes for all functions
  */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
