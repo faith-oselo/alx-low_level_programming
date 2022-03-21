d75c21e4b1f8067b4263cf0ec735468bbd6a20b7
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-21 18:20:12 +0300

    Print the reverse of a given string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+
+	for (l = l - 1; l >= 0; l--)
+		_putchar(s[l]);
+
+	_putchar('\n');
+}
