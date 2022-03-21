e605c1329b210a0d9a7410d502bc28034e3aeb8e
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-21 19:38:02 +0300

    copy a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (*(src + 1) != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
-	}
-
-	for ( ; j < i; j++)
-	{
-		dest[j] = src[j];
+		dest[i] = src[i];
 	}
 	dest[i] = '\0';
 
