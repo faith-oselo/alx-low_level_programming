3827be5b81060ae909ae0a3fcca5b6f0b99bb212
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-28 18:40:16 +0300

    A program that searches a string for any of set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,16 +8,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i;
-	unsigned int j;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-				return (&s[i]);
-		}
+		for (i = 0; accept[i]; i++)
+			if (*s == accept[i])
+				return (s);
+
+		s++;
 	}
 
 	return (NULL);
