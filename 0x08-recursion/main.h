64936cd2a11f7a78dc0014222f6faf2a590567b8
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-03-30 14:49:38 +0300

    A program that prints a string followed by a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions in recursion
+ *	directory
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
