78431d9c27d5420d49eb9360ab1b0b70787ae382
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-04-11 17:46:42 +0300

    A header file for new type struct dog with variables name, age and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,6 @@
 #define DOG_H
 
 /**
- * Desc: Header file that defines a new type struct dog.
- */
-
-/**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
@@ -18,11 +14,7 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for struct dog
- */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
