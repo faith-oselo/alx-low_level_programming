459dff87ab289b6e448cf724ba20f84f49c4d147
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-04-13 16:02:25 +0300

    A program that performs simple mathimatical operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -5,11 +5,6 @@
  * Desc: Header file containing all structures and
  *       prototypes used by the 3-main.c program.
  */
-/**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
- */
 typedef struct op
 {
 	char *op;
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -51,6 +51,8 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+		return (-1);
 	return (a / b);
 }
 
@@ -63,5 +65,7 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+		return (-1);
 	return (a % b);
 }
