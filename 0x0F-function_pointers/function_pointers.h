cd85a4b2a655f691ecefdf64ad0378db98d4d339
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-04-13 16:27:22 +0300

    A function that prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,6 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef HEADERGUARD
+#define HEADERGUARD
 
-/**
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-function_pointers directory.
- */
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
