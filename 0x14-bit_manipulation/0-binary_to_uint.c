6e1411aef70e965b68f8e69c6908c82f26164139
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-04-28 18:56:38 +0300

    Fixing an issue

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * _stoi - converts chars to ints
- * @c: char to convert
- * Return: converted int
- */
-unsigned int _stoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-/**
- * _strlen - calculates the length of the string
- * @s: input
- * Return: length of string
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
-
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-/**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
  *
@@ -32,18 +10,16 @@ unsigned int _strlen(const char *s)
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int result, tmp, expo;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
-	result = tmp = 0;
-	expo = 1;
-	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		tmp = _stoi(b[i]);
-		result += tmp * expo;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (result);
+	return (dec_val);
 }
