a6de0211ba15f0699aaec50f0dfbecf08910f465
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-05-02 14:41:29 +0300

    A function that reads a text file and prints it to the POSIX standard output, a header file for all functions and a putchar function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x15-file_io directory.
- */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
